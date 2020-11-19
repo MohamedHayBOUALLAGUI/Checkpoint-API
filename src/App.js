d2ca556ae6fd4230a443b3da8ece2f55956c5039
MohamedHayBOUALLAGUI <haithem.bouallagui90@gmail.com>
2020-11-20 00:26:13 +0100

    checkpoint-api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import UserList from './Components/UserList/UserList'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div style={{fontSize:'30px',color:'whitesmoke',background:'black'}}><h1>Checkpoint API</h1></div>
+      <div style={{textAlign:'center',margin:'auto 30% auto 30%'}}><UserList/></div>
+      
     </div>
   );
 }
